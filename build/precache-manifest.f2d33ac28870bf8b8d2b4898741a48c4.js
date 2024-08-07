ecdbe1589efc252e1b43cbc66e3192749cc9350b
sancharika <sancharikadebnath@gmail.com>
2024-08-07 20:28:16 +0530

    php file added

---
+++ build/precache-manifest.f2d33ac28870bf8b8d2b4898741a48c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c545a677a40d713823cf3f1d6f6b4b5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15de1aaa368e32567d14",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "06c4dd91b2dbef4dfc4c",
+    "url": "/static/js/2.32448477.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "15de1aaa368e32567d14",
+    "url": "/static/js/main.5f1277b1.chunk.js"
+  },
+  {
+    "revision": "02558d794f1b309512ce",
+    "url": "/static/js/runtime~main.f27e42ef.js"
+  }
+]);
