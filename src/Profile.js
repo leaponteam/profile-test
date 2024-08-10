aa94b2c33bef9439b2da231e8f93e483c1508f2f
sancharika <sancharikadebnath@gmail.com>
2024-08-11 01:04:36 +0530

    modifcation

--- src/Profile.js
+++ src/Profile.js
@@ -11,8 +11,8 @@ function Profile() {
     useEffect(() => {
         const fetchProfileData = async () => {
             try {
-                // const response = await fetch(`http://127.0.0.1:8000/${username}/`);
-                const response = await fetch(`https://leaponapi-test.herokuapp.com/${username}/`);
+                // const response = await fetch(`http://127.0.0.1:8000/user/${username}/`);
+                const response = await fetch(`https://leaponapi-test.herokuapp.com/user/${username}/`);
                 
                 const data = await response.json();
                 setProfileData(data);
