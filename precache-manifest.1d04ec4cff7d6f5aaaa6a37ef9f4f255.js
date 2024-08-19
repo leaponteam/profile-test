bf04edb1e9c27671b8e5eb43ec0864e9feb9564f
Sancharika Debnath <80142595+sancharika@users.noreply.github.com>
2024-08-20 00:31:29 +0530

    Add files via upload

---
+++ precache-manifest.1d04ec4cff7d6f5aaaa6a37ef9f4f255.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f52c2d683eb4eac61e35209bb7df637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e399887f7697408df0d",
+    "url": "/static/js/2.2141cdf0.chunk.js"
+  },
+  {
+    "revision": "4958e402c8180c813a7e",
+    "url": "/static/js/main.8a2f2462.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
