de1b1876cb979dc9385489c21b880f57534a6871
sancharika <sancharikadebnath@gmail.com>
2025-01-28 20:40:02 +0530

    php

--- src/lib/client.js
+++
@@ -1,8 +0,0 @@
-import {createClient} from '@sanity/client'
-
-export const client = createClient({
-  projectId: 'x1rzw5ao',
-  dataset: 'production',
-  apiVersion: '2022-03-07',
-  useCdn: true
-})
